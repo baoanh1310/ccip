02036e19ecfda8770f89c2351123bf42753cb0b4
SotaTek-AnhPham-3 <anh.pham3@sotatek.com>
2023-07-27 17:45:59 +0700

    grant minter role to TokenReceiver

--- tasks/cross-chain-token.ts
+++ tasks/cross-chain-token.ts
@@ -14,8 +14,13 @@ import {
 } from "../typechain-types";
 import { getCcipMessageId } from "./helpers";
 
-task(`deploy-icb-token`, `Deploys the ICBToken smart contract on source/destination blockchain`)
+task(`deploy-token-sender`, `Deploys the TokenSender smart contract`)
+    .addOptionalParam(`router`, `The address of the Router contract`)
+    .addOptionalParam(`link`, `The address of the LINK token`)
     .setAction(async (taskArguments: TaskArguments, hre: HardhatRuntimeEnvironment) => {
+        const routerAddress = taskArguments.router ? taskArguments.router : getRouterConfig(hre.network.name).address;
+        const linkAddress = taskArguments.link ? taskArguments.link : LINK_ADDRESSES[hre.network.name]
+
         const privateKey = getPrivateKey();
         const rpcProviderUrl = getProviderRpcUrl(hre.network.name);
 
@@ -33,44 +38,32 @@ task(`deploy-icb-token`, `Deploys the ICBToken smart contract on source/destinat
         await token.deployed();
 
         spinner.stop();
-        console.log(`✅ ICBToken deployed at address ${token.address} on ${hre.network.name} blockchain`)
-    });
+        console.log(`✅ ICBToken deployed at address ${token.address} on ${hre.network.name} blockchain`);
 
-task(`deploy-token-sender`, `Deploys the TokenSender smart contract`)
-    .addParam(`token`, `The address of ICBToken contract`)
-    .addOptionalParam(`router`, `The address of the Router contract`)
-    .addOptionalParam(`link`, `The address of the LINK token`)
-    .setAction(async (taskArguments: TaskArguments, hre: HardhatRuntimeEnvironment) => {
-        const routerAddress = taskArguments.router ? taskArguments.router : getRouterConfig(hre.network.name).address;
-        const linkAddress = taskArguments.link ? taskArguments.link : LINK_ADDRESSES[hre.network.name]
-        const tokenAddress = taskArguments.token;
-
-        const privateKey = getPrivateKey();
-        const rpcProviderUrl = getProviderRpcUrl(hre.network.name);
-
-        const provider = new providers.JsonRpcProvider(rpcProviderUrl);
-        const wallet = new Wallet(privateKey);
-        const deployer = wallet.connect(provider);
-
-        const spinner: Spinner = new Spinner();
-
-        console.log(`ℹ️  Attempting to deploy TokenSender on the ${hre.network.name} blockchain using ${deployer.address} address, with the Router address ${routerAddress} & LINK address ${linkAddress} & ICBToken address ${tokenAddress} provided as constructor arguments`);
+        console.log(`ℹ️  Attempting to deploy TokenSender on the ${hre.network.name} blockchain using ${deployer.address} address, with the Router address ${routerAddress} & LINK address ${linkAddress} & ICBToken address ${token.address} provided as constructor arguments`);
         spinner.start();
 
         const tokenSenderFactory: TokenSender__factory = await hre.ethers.getContractFactory('TokenSender') as TokenSender__factory;
-        const tokenSender: TokenSender = await tokenSenderFactory.deploy(routerAddress, linkAddress, tokenAddress);
+        const tokenSender: TokenSender = await tokenSenderFactory.deploy(routerAddress, linkAddress, token.address);
         await tokenSender.deployed();
 
         spinner.stop();
-        console.log(`✅ TokenSender deployed at address ${tokenSender.address} on ${hre.network.name} blockchain`)
+        console.log(`✅ TokenSender deployed at address ${tokenSender.address} on ${hre.network.name} blockchain`);
+
+        console.log(`ℹ️  Attempting to give burn permission of ICB Token (${token.address}) for the TokenSender contract ${tokenSender.address} account`);
+        spinner.start();
+
+        const tx = await token.connect(deployer).setBurner(tokenSender.address);
+        await tx.wait();
+
+        spinner.stop();
+        console.log(`ℹ️  Give burn permission of ICB token ${taskArguments.token} to TokenSender success`);
     });
 
 task(`deploy-token-receiver`, `Deploys the TokenReceiver smart contract`)
-    .addParam(`token`, `The address of ICBToken contract`)
     .addOptionalParam(`router`, `The address of the Router contract`)
     .setAction(async (taskArguments: TaskArguments, hre: HardhatRuntimeEnvironment) => {
         const routerAddress = taskArguments.router ? taskArguments.router : getRouterConfig(hre.network.name).address;
-        const tokenAddress = taskArguments.token;
 
         const privateKey = getPrivateKey();
         const rpcProviderUrl = getProviderRpcUrl(hre.network.name);
@@ -81,42 +74,37 @@ task(`deploy-token-receiver`, `Deploys the TokenReceiver smart contract`)
 
         const spinner: Spinner = new Spinner();
 
-        console.log(`ℹ️  Attempting to deploy TokenReceiver on the ${hre.network.name} blockchain using ${deployer.address} address, with the Router address ${routerAddress} and ICBToken address ${tokenAddress} provided as constructor arguments`);
+        console.log(`ℹ️  Attempting to deploy ICBToken on the ${hre.network.name} blockchain using ${deployer.address} address`);
+        spinner.start();
+
+        const tokenFactory: ICBToken__factory = await hre.ethers.getContractFactory('ICBToken') as ICBToken__factory;
+        const token: ICBToken = await tokenFactory.deploy();
+        await token.deployed();
+
+        spinner.stop();
+        console.log(`✅ ICBToken deployed at address ${token.address} on ${hre.network.name} blockchain`);
+
+        console.log(`ℹ️  Attempting to deploy TokenReceiver on the ${hre.network.name} blockchain using ${deployer.address} address, with the Router address ${routerAddress} and ICBToken address ${token.address} provided as constructor arguments`);
         spinner.start();
 
         const tokenReceiverFactory: TokenReceiver__factory = await hre.ethers.getContractFactory('TokenReceiver') as TokenReceiver__factory;
         const tokenReceiver: TokenReceiver = await tokenReceiverFactory.deploy(
             routerAddress, 
-            tokenAddress   
+            token.address   
         );
         await tokenReceiver.deployed();
 
         spinner.stop();
-        console.log(`✅ TokenReceiver deployed at address ${tokenReceiver.address} on ${hre.network.name} blockchain`)
-    });
+        console.log(`✅ TokenReceiver deployed at address ${tokenReceiver.address} on ${hre.network.name} blockchain`);
 
-task(`set-token-sender-as-burner`, `Set TokenSender contract permission to burn ICB Token on source blockchain`)
-    .addParam(`token`, `The address of the ICBToken smart contract on source blockchain`)
-    .addParam(`tokenSender`, `The address of TokenSender contract on source blockchain`)
-    .setAction(async (taskArguments: TaskArguments, hre: HardhatRuntimeEnvironment) => {
-        const rpcProviderUrl = getProviderRpcUrl(hre.network.name);
-        const provider = new providers.JsonRpcProvider(rpcProviderUrl);
-        const privateKey = getPrivateKey();
-        const wallet = new Wallet(privateKey);
-        const signer = wallet.connect(provider);
-
-        const spinner: Spinner = new Spinner();
-
-        const token: ICBToken = ICBToken__factory.connect(taskArguments.token, provider);
-
-        console.log(`ℹ️  Attempting to give burn permission of ICB Token (${taskArguments.token}) for the TokenSender contract ${taskArguments.tokenSender} account`);
+        console.log(`ℹ️  Attempting to grant the minter role to the TokenReceiver smart contract`);
         spinner.start();
 
-        const tx = await token.connect(signer).setBurner(taskArguments.tokenSender);
+        const tx = await token.transferOwnership(tokenReceiver.address);
         await tx.wait();
 
         spinner.stop();
-        console.log(`ℹ️  Give burn permission of ICB token ${taskArguments.token} to TokenSender success`);
+        console.log(`✅ TokenReceiver can now mint ICB. Transaction hash: ${tx.hash}`);
     });
 
 task('cross-chain-icb-balance-of', 'Gets the balance of ICB token for provided address')
